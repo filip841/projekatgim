9b3f05b78c8911806975c1f6e1abadf3207878ad
Filip Milanovic <ficocofi2@gmail.com>
2023-12-21 00:15:15 +0100

    update 1

--- login_skripta.js
+++ login_skripta.js
@@ -2,3 +2,21 @@ document.getElementById("login_forma").addEventListener("submit",function(event)
     event.preventDefault();
     window.location.href = "glavna_stranica.html";
 });
+const singup_link = document.querySelector(".signup-link");
+const email = document.getElementById("emailContainer");
+const submit = document.getElementById("submit");
+const tekst = document.querySelector("#dont_have_account");
+singup_link.addEventListener("click",function(event){
+    event.preventDefault();
+    if(email.style.display==="none" || email.style.display === ''){
+        email.style.display = 'block';
+        submit.textContent = "SIGN UP";
+        tekst.textContent = "Have an account?";
+        singup_link.textContent = "Log in";
+    }else{
+        email.style.display = "none";
+        submit.textContent = "LOG IN";
+        tekst.textContent = "Don't have an account?";
+        singup_link.textContent = "Sign up";
+    }
+});
