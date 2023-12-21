274b93c565a8f0f5d29564ec0c6868665f357f1a
Filip Milanovic <ficocofi2@gmail.com>
2023-12-21 01:24:57 +0100

    neki bagovi kod unosa

--- login_skripta.js
+++ login_skripta.js
@@ -1,11 +1,19 @@
-document.querySelector("#submit").addEventListener("click",function(event){
-    event.preventDefault();
-    window.location.href = "glavna_stranica.html";
-});
 const singup_link = document.querySelector(".signup-link");
 const email = document.getElementById("emailContainer");
 const submit = document.getElementById("submit");
 const tekst = document.querySelector("#dont_have_account");
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+const email_tekst = document.getElementById("email");
+document.querySelector("#submit").addEventListener("click",function(event){
+        if (username.value !== "" && password.value !== "") {
+            if (email.style.display !== "block") {
+                window.location.href = "glavna_stranica.html";
+            } else if (email_tekst.value !== "") {
+                window.location.href = "glavna_stranica.html";
+            }
+        }
+});
 singup_link.addEventListener("click",function(event){
     event.preventDefault();
     if(email.style.display==="none" || email.style.display === ''){
