b86dbf98f6a098d566f93ce2843e4493e5aed483
Filip Milanovic <ficocofi2@gmail.com>
2023-12-31 15:13:21 +0100

    aaaaaa

--- login/main.js
+++ login/main.js
@@ -1,5 +1,4 @@
 const singup_link = document.querySelector(".signup-link");
-<<<<<<< HEAD
 const login_link = document.querySelector(".login-link");
 const email = document.getElementById("emailContainer");
 const submitLOGIN = document.getElementById("submitL");
@@ -12,21 +11,12 @@ const usernameLOGIN = document.getElementById("usernameL");
 const passwordLOGIN = document.getElementById("passwordL");
 const email_tekst = document.getElementById("email");
 const city_dropdown = document.getElementById("gradovi");
-=======
-const email = document.getElementById("emailContainer");
-const submit = document.getElementById("submit");
-const tekst = document.querySelector("#dont_have_account");
-const username = document.getElementById("username");
-const password = document.getElementById("password");
-const email_tekst = document.getElementById("email");
->>>>>>> 7990224f51bc0278baf2417e93be340783e78eb1
 
 const api = 'https://rarog-django.vercel.app/api'
 
 document.querySelector("#login_forma")
     .addEventListener("submit", (event) => {
         event.preventDefault()
-<<<<<<< HEAD
         fetchLogin();
 });
 
@@ -38,30 +28,6 @@ document.querySelector("#signup_forma")
 
 window.addEventListener("DOMContentLoaded",function(){
     fetchCities();
-=======
-        if (username.value !== "" && password.value !== "") {
-            if (email.style.display !== "block") {
-                fetchLogin()
-            } else if (email_tekst.value !== "") {
-                fetchSignUp()
-            }
-        }
-});
-
-singup_link.addEventListener("click",function(event){
-    event.preventDefault();
-    if(email.style.display==="none" || email.style.display === ''){
-        email.style.display = 'block';
-        submit.textContent = "SIGN UP";
-        tekst.textContent = "Already have an account?";
-        singup_link.textContent = "Log in";
-    }else{
-        email.style.display = "none";
-        submit.textContent = "LOG IN";
-        tekst.textContent = "Don't have an account?";
-        singup_link.textContent = "Sign up";
-    }
->>>>>>> 7990224f51bc0278baf2417e93be340783e78eb1
 });
 
 async function fetchSignUp() {
@@ -71,21 +37,12 @@ async function fetchSignUp() {
             method: 'POST',
             body: JSON.stringify({
                 email: email_tekst.value,
-<<<<<<< HEAD
                 username: usernameSIGNUP.value,
                 password: passwordSIGNUP.value
             })
         })
         if(response.ok) {
             const responseParsed = await response.json()
-=======
-                username: username.value,
-                password: password.value
-            })
-        })
-        if(response.ok) {
-            const responseParsed = response.json()
->>>>>>> 7990224f51bc0278baf2417e93be340783e78eb1
             // TODO: I have no idea what does our backend return in this case, but it should be parsed and stored in a cookie or local storage
             window.location.href = "./home/"
         } else {
@@ -103,21 +60,12 @@ async function fetchLogin() {
         {
             method: 'POST',
             body: JSON.stringify({
-<<<<<<< HEAD
                 username: usernameLOGIN.value,
                 password: passwordLOGIN.value
             })
         })
         if(response.ok) {
             const responseParsed = await response.json()
-=======
-                username: username.value,
-                password: password.value
-            })
-        })
-        if(response.ok) {
-            const responseParsed = response.json()
->>>>>>> 7990224f51bc0278baf2417e93be340783e78eb1
             const { id: userId } = responseParsed
             // Add the userId to the cookies so you can access it from the other pages
             document.cookie = `userId=${userId}; max-age=${12 * 60 * 60};`
@@ -129,7 +77,6 @@ async function fetchLogin() {
     } catch(e) {
         console.log(e)
     }
-<<<<<<< HEAD
 }
 
 async function fetchCities() {
@@ -152,6 +99,3 @@ async function fetchCities() {
         console.log(e)
     }
   }
-=======
-}
->>>>>>> 7990224f51bc0278baf2417e93be340783e78eb1
