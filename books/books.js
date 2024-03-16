7a59fe77ca13ee78855cb2d1a5a7f33b8b77cc10
Filip Milanovic <ficocofi2@gmail.com>
2024-03-16 09:24:47 +0100

    test

--- books/books.js
+++ books/books.js
@@ -1,7 +1,18 @@
+import books from './books.json' assert { type: 'json' }
 
-const pictures = document.querySelectorAll(".category-image1");
-    pictures.forEach(function (picture) {
-        picture.addEventListener("click", function () {
-            window.location.href = "idk/";
-        });
-    });
+const items = document.getElementById('items')
+
+let itemsHtml = ``
+
+books.forEach(book => {
+    itemsHtml += `
+    <article>
+      <img src="https://placehold.co/400" alt="cover">
+      <h3>${ book.name }</h3>
+      <p>(${ book.year })</p>
+      <p>${ book.writter }</p>
+    </article>
+    `
+})
+
+items.innerHTML = itemsHtml
--- home/home.js
+++
@@ -1,6 +0,0 @@
-const book = document.getElementById("name_10_books");
-const music = document.getElementById("music");
-const movie = document.getElementById("movie");
-book.addEventListener('click',function(){
-    window.location = "/books";
-});
